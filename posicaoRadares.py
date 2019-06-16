528ac9aca47b772f315c3f779b0edc58885d9a36
Glaysonvisgueira <31600079+Glaysonvisgueira@users.noreply.github.com>
2019-06-16 14:10:10 -0300

    Update posicaoRadares.py

--- posicaoRadares.py
+++ posicaoRadares.py
@@ -8,19 +8,21 @@ Dados coletados no site da prefeitura:
 https://pmt.pi.gov.br
 '''
 
-url = 'https://pmt.pi.gov.br/localizacao-de-radares/'   
+url = 'https://pmt.pi.gov.br/localizacao-de-radares/'      #Página onde são dispostos os arquivos PDFs com as localizações dos radares 
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-buscarLink = soup.find_all(id='core')
-links = buscarLink[0].find_all('h4')
+buscarLink = soup.find_all(id='core')        #Buscando a div com a class: core
+links = buscarLink[0].find_all('h4')         #Buscando dentro da div com a classe core todos os elementos com a tag: h4  
 
 radaresFixos = links[0].find('a')
 radaresEstaticos =  links[1].find('a')
 
-print("FIXOS     " + radaresFixos.get('href'))
-print("ESTÁTICOS " + radaresEstaticos.get('href'))
+print("FIXOS     " + radaresFixos.get('href'))     #Coletando o link dentro da tag: a
+print("ESTÁTICOS " + radaresEstaticos.get('href')) #Coletando o link dentro da tag: a
 
+
+#Utilizando a lib webbrowser para abrir o link direto na aba do navegador padrão do usuário
 webbrowser.open_new_tab(radaresFixos.get('href'))
 webbrowser.open_new_tab(radaresEstaticos.get('href'))
 
@@ -33,4 +35,6 @@ for link in soup.find_all('a'):
     a = link.get('href')
     print(a)
 '''    
+   
+
    
